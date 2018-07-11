0f1e4582bb5ec2e0630e8dac85cadb42baeaa184
bneal3 <bengneal3@gmail.com>
2018-07-11 13:29:28 -0700

    Add Pokemon and PokemonCell classes

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Benjamin Neal on 1/8/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias  DownloadComplete = () -> ()
